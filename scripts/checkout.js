ea98d1fb1d2e779f37aba94df0e5ebcd7fb9a12b
Arv9885 <arvindmangalarapu463@gmail.com>
2024-01-11 15:42:55 +0530

    added class and to remove products using .remove

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,8 @@ cart.forEach((cartItem) => {
 
   console.log(matchingProduct);
 
-  cartSummaryHTML += `   <div class="cart-item-container">
+  cartSummaryHTML += ` 
+            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -98,6 +99,7 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
 
-    
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
