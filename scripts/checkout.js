cba4cc67879a74411d97c1240184f658f7f9714b
Arv9885 <arvindmangalarapu463@gmail.com>
2024-01-12 12:22:50 +0530

    added update html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart,updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
@@ -100,7 +100,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
-    html += `         <div class="delivery-option">
+    html += `         
+          <div class="delivery-option js-delivery-option"
+          data-product-id="${matchingProduct.id}"
+          data-delivery-option-id ="${deliveryOption.id}">
             <input
                 type="radio"
                 ${isChecked ? "checked" : ""}
@@ -109,7 +112,8 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
             />
             <div>
                 <div class="delivery-option-date">${dateString}</div>
-                <div class="delivery-option-price">${priceString} Shipping</div>
+                <div class="delivery-option-price">${priceString} Shipping
+                </div>
             </div>
          </div>`;
   });
@@ -128,4 +132,17 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     );
     container.remove();
   });
+});
+
+
+
+document.querySelectorAll(".js-delivery-option")
+.forEach((element)=>{
+element.addEventListener('click',()=>{
+    // const productId = element.dataset.productId;
+    // const deliveryOptionId =element.dataset.deliveryOptionId;
+// shortcut
+    const{productId,deliveryOptionId}= element.dataset;
+updateDeliveryOption(productId,deliveryOptionId);
+})
 });
