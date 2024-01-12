ff2a24a2daec280547ff46c40c8909254a8513a1
Arv9885 <arvindmangalarapu463@gmail.com>
2024-01-12 10:33:00 +0530

    changes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,17 @@
 import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { deliveryOptions } from "../data/deliveryOptions.js";
 
 let cartSummaryHTML = "";
+
+hello();
+const today = dayjs();
+const deliveryDate = today.add(7, "days");
+
+console.log(deliveryDate);
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -18,7 +27,9 @@ cart.forEach((cartItem) => {
   console.log(matchingProduct);
 
   cartSummaryHTML += ` 
-            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+            <div class="cart-item-container js-cart-item-container-${
+              matchingProduct.id
+            }">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -92,6 +103,26 @@ cart.forEach((cartItem) => {
           </div>`;
 });
 
+function deliveryOptionsHTML() {
+  deliveryOptions.forEach(() => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+    const dateString = deliveryDate.format("dddd, MMMM d");
+     
+    `         <div class="delivery-option">
+            <input
+                type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${matchingProduct.id}"
+            />
+            <div>
+                <div class="delivery-option-date">Wednesday, June 15</div>
+                <div class="delivery-option-price">$4.99- Shipping</div>
+            </div>
+         </div>`;
+  });
+}
+
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
 document.querySelectorAll(".js-delete-link").forEach((link) => {
@@ -99,7 +130,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
 
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
     container.remove();
   });
 });
